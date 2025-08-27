97363319af7b502dd4fb149dd002e1b39e5f63b7
Kaiji <stgorbissi@gmail.com>
2025-08-27 22:37:30 +0300

    feat: initial project structure

---
+++ src/main/java/org/barbershop/barbershopbot/BarbershopBotApplication.java
@@ -0,0 +1,13 @@
+package org.barbershop.barbershopbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarbershopBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/barbershop/barbershopbot/BarbershopBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.barbershop.barbershopbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
